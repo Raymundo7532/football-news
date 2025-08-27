0d1ff4c8691325adea7a959e86458ec88cbab8d5
Raymundo7532 <woloblocrafael@gmail.com>
2025-08-27 16:33:04 +0700

    kesalahan

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,8 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "<URL deployment PWS kamu>"]
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raymundo-rafaelito-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
